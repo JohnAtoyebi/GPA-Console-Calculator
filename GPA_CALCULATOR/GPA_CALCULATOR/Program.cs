cb05e08fd743d8939e3951977bc955b9d97e5d09
unknown <atoyebijohn1@gmail.com>
2022-09-23 13:40:21 +0100

    first commit

---
+++ GPA_CALCULATOR/GPA_CALCULATOR/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace GPA_CALCULATOR
+{
+    internal class Program
+    {   
+        public static void WelcomeInfo()
+        {
+            Console.WriteLine();
+            Console.WriteLine("WELCOME TO ATOYEBI JOHN'S CONSOLE GPA CALCULATOR");
+            Console.WriteLine("-----------------I'm a DecaDev-----------------");
+            Console.WriteLine();
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey(true);
+        }
+        static void Main(string[] args)
+        {
+            WelcomeInfo();
+
+            UserInterface userInterface = new UserInterface();
+
+            Console.WriteLine("Type help to show options");
+            Console.Write(">");
+            string inputLine = Console.ReadLine().Trim();
+
+            while (!inputLine.Equals("") && !inputLine.Equals("exit"))
+            {
+                if (inputLine.ToLower().Equals("add"))
+                {
+                    userInterface.StartUpDisplay();
+                }
+                else if (inputLine.ToLower().Equals("print"))
+                {
+                    userInterface.DisplayResult();
+                }
+                else if (inputLine.ToLower().Equals("help"))
+                {
+                    userInterface.Help();
+                }
+
+                else
+                {
+                    Console.WriteLine("Command not recognised, type help to see all options");
+                }
+
+                Console.Write(">");
+
+                inputLine = Console.ReadLine();
+
+                Console.Clear();
+            }
+        }
+    }
+    
+}
